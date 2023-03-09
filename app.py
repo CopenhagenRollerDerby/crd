5a56b073746b57ab2ddc67fd3364a8e817e8d1c4
jagenau <sarah@radicalpet.net>
2023-03-09 19:09:50 +0100

    somewhat working

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    home = os.listdir('static/img/home')
+    away = [f'img/away/{file}' for file in os.listdir('static/img/away')]
+    print(away)
+    return render_template('index.html', away=away, home=home)
